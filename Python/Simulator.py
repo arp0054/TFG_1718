741d43a4da4badd28d201ad6ed29efb1c99272e4
Unknown <arp0054@alu.ubu.es>
2019-01-09 20:01:14 +0100

    Solución de errores
    Solución de errores en el simulador del láser

--- Python/Simulator.py
+++ Python/Simulator.py
@@ -10,7 +10,7 @@ import threading
  
 def conexiones(socket_cliente):
     peticion = socket_cliente.recv(1024)
-    print "[*] Mensaje recibido: %s" % peticion
+    print ("[*] Mensaje recibido: %s" % peticion)
     socket_cliente.send("HOLA CLIENTE")
     socket_cliente.close()
 
@@ -25,11 +25,11 @@ servidor.bind((ip, puerto))
 servidor.listen(max_conexiones)
 
 
-print "[*] Esperando conexiones en %s:%d" % (ip, puerto)
+print ("[*] Esperando conexiones en %s:%d" % (ip, puerto))
 
 
 while True:
     cliente, direccion = servidor.accept()
-    print "[*] Conexion establecida con %s:%d" % (direccion[0] , direccion[1])
+    print ("[*] Conexion establecida con %s:%d" % (direccion[0] , direccion[1]))
     conexiones = threading.Thread(target=conexiones, args=(cliente,))
     conexiones.start()
